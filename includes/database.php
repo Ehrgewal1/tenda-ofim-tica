df24dc41e821bf5f03939cf2ce7a40a19e11294c
Ehrgewal1 <jmorenor@esliceu.net>
2022-04-06 11:13:26 +0200

    1.1

--- includes/database.php
+++ includes/database.php
@@ -5,6 +5,7 @@ if (!$dbh) {
     echo "Error db_connect: error en mysqli_connect user: (mal usuario/password?)<br>";
     die;
 }
+else
 
 if (!mysqli_select_db($dbh, 'test')) {
     echo "Error db_connect: error en mysqli_selectdb: _BBDD_NAME<br>";
