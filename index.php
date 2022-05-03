3328dd231a47f0fafc4ee729e5f0d09fee716aeb
Cristina <cnavarrog@esliceu.net>
2022-05-03 10:18:55 +0200

    index

--- index.php
+++ index.php
@@ -3,9 +3,35 @@
     <link rel="stylesheet" href="styles.css">
     <?php include 'includes/head.php';?>
     <body> 
-        <h1>Welcome</h1>
-        <p>Text</p>
-        <?php include 'includes/header.php';?>
-        <?php include 'includes/footer.php';?>
-    </body>
+    <?php include 'includes/header.php';?>
 </html>
+        <h1>Welcome</h1>
+        <p>tabla de clients</p>
+<selection>
+</selection>
+<table>
+    <thead>
+        <tr>
+            <td>ID</td>
+            <td>NOM</td>
+            <td>PAIS</td>
+            <td>CP</td>
+        </tr>
+    </thead>
+<tbody>
+    <?php
+    $query = "SELECT * FROM policia";
+    $result = mysqli_query($dbh, $query) or die(mysqli_error($dbh));
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+            <td>".$row['id']."</td>
+            <td>".$row['nom']."</td>
+            <td>".$row['pais']."</td>
+            <td>".$row['cp']."</td>
+    </tr>";
+    }
+    ?>
+</tbody>
+</table>
+<?php include 'includes/footer.php';?>
+    </body>
