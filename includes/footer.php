0fb219736addfa384b30a4a384869976ac571923
Cristina <cnavarrog@esliceu.net>
2022-06-01 10:50:43 +0200

    m

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,5 @@
 <footer>
-    <p>Autor: Cristina </p>
-    <p><a href="mailto:proba@example.com">proba@example.com</a></p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </footer>
 
 
